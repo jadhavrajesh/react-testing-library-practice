1a5f0691843fb38de5232f6df43cac2d83cdecd7
Rajesh Jadhav <rajeshjadhav.rj@gmail.com>
2023-02-26 18:08:53 +0530

    added jest-dom plugin for eslint to add few more rules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Skills } from "./components/Skills/Skills";
-import { skills as SkillsData } from "./components/Skills/data/";
-import { Application } from "./components/Application/Application";
+// import { Skills } from "./components/Skills/Skills";
+// import { skills as SkillsData } from "./components/Skills/data/";
+// import { Application } from "./components/Application/Application";
 import Users from "./components/Users/Users";
 
 function App() {
