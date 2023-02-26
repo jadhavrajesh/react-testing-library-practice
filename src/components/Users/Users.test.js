46bc663415cd2d6fb2d0773bd9878350160fa768
Rajesh Jadhav <rajeshjadhav.rj@gmail.com>
2023-02-26 17:00:27 +0530

    mock error responses with MSW

--- src/components/Users/Users.test.js
+++ src/components/Users/Users.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
 import Users from "./Users";
+import { server } from "../../mocks/server";
 
 describe("Users", () => {
   test("renders correctly", () => {
@@ -12,5 +14,20 @@ describe("Users", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("renders error", async () => {
+    // reset the main handler here to throw an error
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const errorText = await screen.findByText("Error while fetching Users");
+    expect(errorText).toBeInTheDocument();
   });
 });
